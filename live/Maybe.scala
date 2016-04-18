520e3f0cde185f7f3fd19f7d5faf8dd5de1e2989
armundle <amundle@cognitivescale.com>
2016-04-18 13:36:58 -0500

    Add append to Maybe

--- live/Maybe.scala
+++ live/Maybe.scala
@@ -10,10 +10,28 @@ sealed abstract class Maybe[A] {
 }
 
 object Maybe {
+
+  def empty[A]: Maybe[A] = Empty()
+
+  def just[A](a: A): Maybe[A] = Just(a)
+ 
   def fold[A, B](o: Maybe[B])(ifEmpty: A)(ifJust: B => A): A =
     o match {
       case Empty() => ifEmpty
       case Just(a) => ifJust(a)
+    }
+
+  implicit def maybeSemigroup[A : Semigroup]: Semigroup[Maybe[A]] =
+    new Semigroup[Maybe[A]]{
+      def append(a: Maybe[A], b: Maybe[A]): Maybe[A] = 
+        a match {
+          case Empty() => b
+          case Just(aa) =>
+            b match{
+              case Empty() => a
+              case Just(bb) => just(Semigroup[A].append(aa, bb))
+            }
+        }
     }
 }
 
