178bae6d106451fe0f815e4e29684626aa2714c2
Alexis <araykhel@cognitivescale.com>
2016-04-27 13:25:10 -0500

    Added syntax for disjunction to create left/right disjunctions.

--- live/src/main/scala/ExampleApp.scala
+++ live/src/main/scala/ExampleApp.scala
@@ -1,32 +1,28 @@
 package com.c12e.learn
 
 
-//import SemigroupSyntax._
+import com.c12e.learn.data.Nel
+import com.c12e.learn.data.\/.Syntax._
+import com.c12e.learn.typeclass.Semigroup
+import com.c12e.learn.typeclass.Semigroup.Syntax._
+import com.c12e.learn.stdlib.StringInstances._
 
 
 object ExampleApp extends App {
 
-  /*
   println(implicitly[Semigroup[String]].append("a", "b"))
+
   println(Semigroup[String].append("a", "b"))
 
   // silly, but possible (explicitly passing an implicit parameter)
-  println(Semigroup.apply(Semigroup.stringSemigroup).append("a", "b"))
+  println(Semigroup.apply(stdlib.StringInstances.stringMonoid).append("a", "b"))
 
   println("a" |+| "b")
 
-  println(new SemigroupOps("a") |+| "b")
+  println(new Semigroup.Ops("a") |+| "b")
 
   println(Nel(1,2,3) |+| Nel(4,5,6))
 
-  Good properties for a type class
-  --------------------------------
-  - global uniqueness of instances for a type
-  - lawful (good type classes have laws)
-  - multiple instances can be made
-  - each function helps derive more functions (data type independently)
-  - automatic derivation of new instances
-  */
-
+  println(1.right[String])
 
 }
--- live/src/main/scala/data/Disjunction.scala
+++ live/src/main/scala/data/Disjunction.scala
@@ -13,4 +13,22 @@ object \/ {
   def left[A, B](a: A): A \/ B = -\/(a)
   def right[A, B](b: B): A \/ B = \/-(b)
 
+  final class Ops[A](val a: A) extends AnyVal {
+    def right[B]: B \/ A = \/-(a)
+    def left[B]: A \/ B = -\/(a)
+  }
+
+  trait Syntax {
+    implicit def any2DisjOps[A](a: A): Ops[A] =
+      new Ops(a)
+  }
+
+  object Syntax extends Syntax
+
 }
+
+/*
+-\/[String, Foo](new Foo)
+\/.right[String, Foo](new Foo)
+(new Foo).right[String]
+*/
