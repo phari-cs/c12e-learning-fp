c054181d736f341369963fd8f3d954bc0d37c002
Andy Beck <abeck@cognitivescale.com>
2016-05-02 13:35:35 -0500

    Functor ints for \/, fold, map.

--- live/src/main/scala/data/Disjunction.scala
+++ live/src/main/scala/data/Disjunction.scala
@@ -1,10 +1,22 @@
 package com.c12e.learn
 package data
 
+
 import com.c12e.learn.typeclass.Functor
 
 
-sealed trait \/[A, B]
+sealed trait \/[A, B] {
+
+  def fold[C](ifLeft: A => C)(ifRight: B => C): C =
+    this match {
+      case -\/(a) => ifLeft(a)
+      case \/-(b) => ifRight(b)
+    }
+
+  def map[C](f: B => C): A \/ C =
+    fold[A \/ C](-\/(_))(f andThen \/-.apply)
+
+}
 
 final case class -\/[A, B](a: A) extends (A \/ B)
 final case class \/-[A, B](b: B) extends (A \/ B)
@@ -29,7 +41,8 @@ object \/ {
 
   implicit def functor[A]: Functor[A \/ ?] =
     new Functor[A \/ ?] {
-
+      def map[B, C](fb: A \/ B)(f: B => C): A \/ C =
+        fb map f
     }
 
 }
