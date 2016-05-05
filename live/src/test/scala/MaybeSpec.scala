18a22379ba310ab5b6e38f07d1d66c11a9f1d039
Sukant Hajra <shajra@cognitivescale.com>
2016-05-05 00:13:29 -0500

    maybe.functor tests

--- live/src/test/scala/MaybeSpec.scala
+++ live/src/test/scala/MaybeSpec.scala
@@ -3,10 +3,11 @@ package test
 
 
 import com.c12e.learn.data.{ Max, Maybe }
-import com.c12e.learn.stdlib._
+import com.c12e.learn.stdlib.instances._
 
 
 object MaybeSpec extends Spec("maybe") {
   checkAll(Props.equal[Maybe[String]])
   checkAll(Props.semigroup[Maybe[Max[Int]]])
+  checkAll(Props.functor[Maybe, Int, String, (Int, String)])
 }
